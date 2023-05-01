13091f29da753908281f30fdb773def31b892223
Elmaschidito3 <fortijc@gmail.com>
2023-05-01 12:07:40 -0600

    creacion del templete

---
+++ Pasteleria.js
@@ -0,0 +1,19 @@
+const {Schema,model}=require('mongoose');
+const pasteleriaSchema=new Schema({
+    codigobarras:{
+        require:true,//se solicita y lo inserta si esta
+        unique:true,// si se duplicaca tampoco se inserta
+        type:String
+    },
+    sabor:String,
+    kilos:Number,
+    descripcion:String,
+    color:String,
+    diseño:String,
+    precioventa:Number
+},{
+    versionkey:false,
+    timestamps:true
+});
+
+module.exports=model('pasteleria',pasteleriaSchema);
---
+++ index.js
@@ -0,0 +1,26 @@
+const express=require('express');
+const morgan=require('morgan');
+const mongoose=require('mongoose');
+const Pasteleria=require('./Pasteleria');
+const app=express();
+
+//Settings 
+app.set('port',process.env.PORT||3600);
+app.set('view engine', 'ejs');
+
+
+//Middlewares
+app.use(morgan('dev'));
+app.use(express.json()); 
+app.use(express.urlencoded({extended:false}));
+
+//Conexion a mongodb atlas
+mongoose.connect("mongodb+srv://fortijc:0987CXZ-He@cluster0.knyyyxf.mongodb.net/Papeleriadb?retryWrites=true&w=majority")
+.then(db=> console.log("Mongodb Connected"))
+.catch(err=>console.error(err));
+
+
+
+app.listen(app.get('port'),()=>{
+    console.log('servidor escuchando en el puerto 3600');
+});
